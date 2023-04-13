fcda3c5f81ca3c4508110968637a4854395f7071
eliasfreeman <oladimejiboluwaji@gmail.com>
2023-04-13 19:56:36 +0100

    new

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ console.log(questions);
 questions.forEach((question) => {
   question.addEventListener('click', (e) => {
     // console.log(e.currentTarget);
+    if (e.currentTarget.classList.contains('show')) {
+      e.currentTarget.classList.remove('show');
+    }
     for (let question of questions) {
       question.classList.remove('show');
     }
